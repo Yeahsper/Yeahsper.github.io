55563ec87c5a32a7f85808ea67c6c1b5bb1a6b91
Jesper Saastamoinen <Yeahsper@gmail.com>
2022-08-06 15:21:57 +0200

    init commit

---
+++ replace_url.js
@@ -0,0 +1,15 @@
+const path = "D:/reignofterrorgd/";
+
+function removeHttp(url) {
+	return url.replace("https://www.reignofterrorgd.com", "");
+}
+function changeUrl() {
+	var links = document.links;
+
+	for (var i = 0, iLen = links.length; i < iLen; i++) {
+		links[i].href = path + removeHttp(links[i].href) + "/index.html";
+	}
+	console.log(links);
+}
+
+changeUrl();
